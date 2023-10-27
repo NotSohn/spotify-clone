12adf485e5119fcb7d8b3d1416bdd8db0fe9af87
Justin Sohn <sohnjustin2@gmail.com>
2023-10-27 03:23:21 -0700

    added login authentication

--- providers/SupabaseProvider.tsx
+++ providers/SupabaseProvider.tsx
@@ -1,7 +1,9 @@
 "use client";
+
 import { useState } from "react";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
+
 import { Database } from "@/types_db";
 
 interface SupabaseProviderProps {
